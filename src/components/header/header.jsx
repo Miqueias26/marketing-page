b4af67443f2b2cd5ad54fff2b5a320e56515640b
miqueias199808@outlook.com <miqueias199808@outlook.com>
2024-12-18 10:11:58 +0100

    Updapte 3.0.0

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,73 +2,60 @@ import "./header.css";
 import JMDevs from "../../assets/img/JM AgencyOF-Photoroom.png";
 import { IoMenuOutline } from "react-icons/io5";
 import { useState } from "react";
+
 function Header() {
   const [showNav, setShowNav] = useState(false);
 
-  const handleOpen = () => {
-    setShowNav(true);
-  };
-
-  const handleCloser = () => {
-    setShowNav(false);
+  const handleToggle = () => {
+    setShowNav((prev) => !prev);
   };
 
   return (
     <div className="navbar">
-      {showNav && (
-        <div className="toogle-model">
-          <div className="toogle-model-menu">
-            <ul>
-              <li>
-                {" "}
-                <a href="">Home</a>
-              </li>
-              <li>
-                {" "}
-                <a href="">Services</a>
-              </li>
-              <li>
-                {" "}
-                <a href="">Team</a>
-              </li>
-              <li>
-                {" "}
-                <a href="">Partners</a>
-              </li>
-              <li>
-                {" "}
-                <a href="">About Us</a>
-              </li>
-            </ul>
-            <button onClick={handleCloser} className="toogle-close">
-              fechar
-            </button>
-          </div>
-        </div>
-      )}
       <div className="logo">
         <img src={JMDevs} alt="JM Devs logo" />
       </div>
+
+      <div className={`toogle-model ${showNav ? "show" : ""}`}>
+        <div className="toogle-model-menu">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Services</a>
+            </li>
+            <li>
+              <a href="">Team</a>
+            </li>
+            <li>
+              <a href="">Partners</a>
+            </li>
+            <li>
+              <a href="">About Us</a>
+            </li>
+          </ul>
+          <button onClick={handleToggle} className="toogle-close">
+            Close
+          </button>
+        </div>
+      </div>
+
       <div className="links-content">
         <ul>
           <li>
-            {" "}
             <a href="">Home</a>
           </li>
           <li>
-            {" "}
             <a href="">Services</a>
           </li>
           <li>
-            {" "}
             <a href="">Team</a>
           </li>
           <li>
-            {" "}
             <a href="">Partners</a>
           </li>
           <li>
-            {" "}
             <a href="">About Us</a>
           </li>
         </ul>
@@ -78,13 +65,12 @@ function Header() {
         <a href="" className="login">
           Login
         </a>
-
         <a href="" className="account">
           Beginner
         </a>
 
         {!showNav && (
-          <button href="" className="toogle-menu" onClick={handleOpen}>
+          <button className="toogle-menu" onClick={handleToggle}>
             <IoMenuOutline />
           </button>
         )}
@@ -92,4 +78,5 @@ function Header() {
     </div>
   );
 }
+
 export default Header;
